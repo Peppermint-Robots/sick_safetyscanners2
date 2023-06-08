25ada344bca3e1a7513787fe301cc6c0e2a9b64e
Syed Faraaz Alam <syed.faraaz@getpeppermint.co>
2023-06-08 10:56:40 +0530

    Updated files.

--- launch/sick_safetyscanners2_launch.py
+++ launch/sick_safetyscanners2_launch.py
@@ -1,33 +1,30 @@
+import os
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ppmt_nav_common import common_utils
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package="sick_safetyscanners2",
-            executable="sick_safetyscanners2_node",
-            name="sick_safetyscanners2_node",
-            output="screen",
-            emulate_tty=True,
-            parameters=[
-                {"frame_id": "scan",
-                 "sensor_ip": "192.168.1.11",
-                 "host_ip": "192.168.1.9",
-                 "interface_ip": "0.0.0.0",
-                 "host_udp_port": 0,
-                 "channel": 0,
-                 "channel_enabled": True,
-                 "skip": 0,
-                 "angle_start": 0.0,
-                 "angle_end": 0.0,
-                 "time_offset": 0.0,
-                 "general_system_state": True,
-                 "derived_settings": True,
-                 "measurement_data": True,
-                 "intrusion_data": True,
-                 "application_io_data": True,
-                 "use_persistent_config": False,
-                 "min_intensities": 0.0}
-            ]
-        )
-    ])
+    ld = LaunchDescription()
+    package_dir = get_package_share_directory("robot_params")
+
+    sick_params_yaml_file = os.path.join(package_dir, "config", "sick_lidar_params.yaml")
+
+    #read yaml file
+    sick_params = common_utils.read_yaml_file(sick_params_yaml_file)
+    sick_lidar_params = sick_params["SickSafetyscannersRos2"]["ros__parameters"]
+
+    for sick_plugin in sick_lidar_params["sick_lidar_plugins"]:
+        sick_node = Node(
+                package="sick_safetyscanners2",
+                executable="sick_safetyscanners2_node",
+                name=sick_plugin + "_sick_safetyscanners2_node",
+                output="screen",
+                namespace=common_utils.get_robot_name(),
+                emulate_tty=True,
+                remappings=[("scan",sick_lidar_params[sick_plugin]["topic"])],
+                parameters=[sick_lidar_params[sick_plugin]]
+            )
+        ld.add_action(sick_node)
+    
+    return ld
